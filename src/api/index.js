7a4d615bac64c0564887c611621b2cb687aef2a2
dxq <ahxqding@163.com>
2020-08-04 20:02:32 +0800

    顶部时间,天气,用户,菜单项动态显示

--- src/api/index.js
+++ src/api/index.js
@@ -12,6 +12,19 @@ export const reqLogin = (username,password) =>ajax(BASE+'/login',{username,passw
 export const reqAddUser = (user) =>ajax(BASE+'/manger/user/add',user,'POST');
 
 // jsonp请求的接口请求函数
+/**
+ * jsonp解决ajax跨域的原理
+ *    1.jsonp只能解决GET类型的ajax请求跨域问题
+ *    2.jsonp请求不是ajax请求，而是一般的GET请求
+ *    3.基本原理：
+ *     浏览器端：
+ *         动态生成<script>来请求后台接口(src就是接口的url) 
+ *         定义好用于接收相应数据的参数(fn)，并将函数名通过请求参数提交给后台(如：callback=fn)
+ *     服务器端：
+ *         接收到请求处理产生结果数据后，返回一个函数调用的js代码，并将结果数据作为实参传入函数调用
+ *     浏览器端：
+ *         收到相应自动执行函数调用的js代码，也就是执行了提前定义好的回调函数，并得到了需要的结果数据
+ */
 export const reqWeather =(city) =>{
     return new Promise((resolve,reject)=>{
         const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
@@ -22,7 +35,7 @@ export const reqWeather =(city) =>{
             if(!err && data.status==='success'){
                 //取出需要的数据
                 const {dayPictureUrl,weather} = data.results[0].weather_data[0];
-                resolve(dayPictureUrl,weather);
+                resolve({dayPictureUrl,weather});
             }else{
                 // 失败
                 message.error('获取天气信息失败');
@@ -30,4 +43,4 @@ export const reqWeather =(city) =>{
         })
     })
 }
-reqWeather('北京')
+//  reqWeather('北京')
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,107 @@
 import React,{Component} from 'react';
 import './index.less';
+import {formatDate} from '../../utils/dateUtils';
+import memoryUtils from '../../utils/memoryUtils';
+import {reqWeather} from '../../api';
+import {withRouter} from 'react-router-dom';
+import menuList from '../../config/menuConfig';
+import {Modal} from 'antd';
+
+
+const { confirm } = Modal;
 /**
  * 头部导航组件
  */
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            currentTime:formatDate(Date.now()),//获取当前时间字符串
+            dayPictureUrl:'',//天气图片
+            weather:'',//天气文本
+         };
     }
+
+    // 获取当前时间
+    getTime = () =>{
+        //每隔一秒获取当前时间,并更新状态数据currentTime
+        setInterval(()=>{
+            const currentTime = formatDate(Date.now());
+            this.setState({
+                currentTime:currentTime
+            })
+        },1000)
+    }
+
+    // 获取实时天气
+    getWeather = async () =>{
+        //调用接口请求函数,异步获取数据
+        const {dayPictureUrl,weather} = await reqWeather('北京');
+        console.log(dayPictureUrl);
+        console.log(weather);
+        this.setState({
+            dayPictureUrl:dayPictureUrl,
+            weather:weather
+        })
+    }
+
+    // 获取当前菜单项的title
+    getTitle = () =>{
+        //得到当前请求路径
+        //!!!这里注意,由于当前组件是非路由组件,没有location,所以需要引入withRouter将Header组件进行包装,暴露新的包装组件
+        const path = this.props.location.pathname;
+        let title;
+        menuList.forEach(item =>{
+            if(item.key===path){//如果当前item对象的key与path一样,item的title就是需要显示的title
+                title = item.title;
+            }else if(item.children){
+                //在所有子item中查找匹配的
+                const cItem = item.children.find(cItem =>cItem.key===path);
+                //如果有值才说明有匹配的
+                if(cItem){
+                    //取出title
+                    title = cItem.title;
+                }
+            }
+        })
+        return title;
+    }
+
+    //退出登录
+    logout = () =>{
+        //显示确认框
+        confirm({
+            content: '确认退出吗?',
+            onOk = () =>{
+                //删除保存的user数据
+                //跳转到login,这里需要重新指定this指向
+                this.props.history.replace('/login');
+            }
+        });
+    }
+
+    //每秒更新一下时间,在第一次render之后执行,一般在此执行异步操作.1:发ajax请求;2.启动定时器
+    componentDidMount(){
+        this.getTime();//获取当前时间
+        this.getWeather();//获取当前天气
+    }
+
     render() {
+        const {currentTime,dayPictureUrl,weather} =this.state;//得到当前时间,天气信息
+        const username = memoryUtils.user.username;//得到当前登录的用户名
+        const title = this.getTitle();//得到当前需要显示的title
         return (
             <div className="header">
                 <div className="header-top">
-                    <span>欢迎：admin</span>
-                    <a href="javascript:">退出</a>
+                    <span>欢迎：{username}</span>
+                    <a href="javascript:" onClick={this.logout}>退出</a>
                 </div>
                 <div className="header-bottom">
-                    <div className="header-bottom-left">首页</div>
+                    <div className="header-bottom-left">{title}</div>
                     <div className="header-bottom-right">
-                        <span>2019-5-16 10:12:36</span>
-                        <img src="fdsfds" alt=""/>
-                        <span>晴</span>
+                        <span>{currentTime}</span>
+                        <img src={dayPictureUrl} alt="weather"/>
+                        <span>{weather}</span>
                     </div>
                 </div>
             </div>
@@ -28,4 +109,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default withRouter(Header);
