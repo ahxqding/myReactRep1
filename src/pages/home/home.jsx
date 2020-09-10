6867553ae7a098c5322e52886b75d345319c7391
dxq <ahxqding@163.com>
2020-09-10 15:59:19 +0800

    首页组件暂时先用文本代替

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './home.less';
 /**
  * 首页路由
  */
@@ -9,8 +10,8 @@ class Home extends Component {
     }
     render() {
         return (
-            <div>
-                首页路由
+            <div className='home'>
+                欢迎使用硅谷后台管理系统
             </div>
         );
     }
