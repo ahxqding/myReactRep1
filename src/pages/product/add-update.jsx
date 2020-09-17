45dc96a6887b2f59ef9cd71bdaa688095a6c015e
dxq <ahxqding@163.com>
2020-09-17 18:24:45 +0800

    商品添加

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, Form, Select, Input, Cascader, Upload, Button, Icon } from 'antd';
 import LinkButton from '../../components/link-button';
 import { reqCategorys } from '../../api/index';
+import PicturesWall from './picturesWall';
 const { Item } = Form;
 const { TextArea } = Input;
 /**
@@ -13,29 +14,48 @@ class ProductAddUpdate extends Component {
         this.state = {
             options: []
         };
+        // 创建用来保存ref标识的标签对象的容器
+        this.pw = React.createRef();
     }
 
     // 初始化options
-    initOptions = (categorys) => {
+    initOptions = async (categorys) => {
         // 根据categorys生成options数组
         const options = categorys.map(c => ({
             value: c._id,
             label: c.name,
             isLeaf: false
         }));
-        this.setState({options});
+
+        // 如果是一个二级分类商品的更新
+        const{ pCategoryId, categoryId } = this.product;
+        if(this.isUpdate && pCategoryId !== '0'){
+            // 获取对应的二级分类列表
+            const subCategorys = await this.getCategotys(pCategoryId);
+            // 生成二级下拉列表的options
+            const childOptions = subCategorys.map(c => ({
+                value: c._id,
+                label: c.name,
+                isLeaf: true
+            }));
+            // 找到当前商品对应的一级option对象
+            const targetOption = options.find(option => option.value === pCategoryId);
+            // 关联到对应的一级option上
+            targetOption.children = childOptions;
+        }
+        this.setState({ options });
     }
 
     // 获取一级/二级分类列表
     // async函数的返回值是一个新的promise对象，promise结果和值又async的结果来决定
     getCategotys = async (partentyId) => {
         const result = await reqCategorys(partentyId);
-        if(result.status === 0){
+        if (result.status === 0) {
             const categorys = result.data;
             // 如果是一级分类列表
-            if(partentyId === '0'){
+            if (partentyId === '0') {
                 this.initOptions(categorys);
-            }else{// 二级列表
+            } else {// 二级列表
                 return categorys; // 返回二级列表 ==>当前async函数返回的promise就会成功且value为categorys
             }
         }
@@ -51,7 +71,7 @@ class ProductAddUpdate extends Component {
         const subCategorys = await this.getCategotys(targetOption.value);
         // 隐藏loading
         targetOption.loading = false;
-        if(subCategorys && subCategorys.length > 0) {
+        if (subCategorys && subCategorys.length > 0) {
             // 生成一个二级列表的options
             const childOptions = subCategorys.map(c => ({
                 value: c._id,
@@ -60,14 +80,14 @@ class ProductAddUpdate extends Component {
             }));
             // 关联到当前options上
             targetOption.children = childOptions;
-        }else{//当前选中的分类没有二级分类
+        } else {//当前选中的分类没有二级分类
             targetOption.isLeaf = true;
         }
         // 更新options
         this.setState({
             options: [...this.state.options],
-          });
-      };
+        });
+    };
 
     validatorPrice = (rule, value, callback) => {
         if (value * 1 <= 0) {
@@ -81,13 +101,25 @@ class ProductAddUpdate extends Component {
         // 进行表单验证，如果通过了，才发送请求
         this.props.form.validateFields((err, values) => {
             if (!err) {
+                console.log(values);
+                const imgs = this.pw.current.getImgs();
+                console.log('imgs', imgs);
                 alert('发送请求');
             }
         })
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.getCategotys('0');
+    }
+
+    componentWillMount() {
+        // 取出携带的state
+        const product = this.props.location.state;// 如果是添加，product没值，否则有值
+        // 保存一个是否更新的标识 !!强行转换成boolean
+        this.isUpdate = !!product;
+        // 保存商品，如果没有，保存空对象，避免报错
+        this.product = product || {};
     }
 
     render() {
@@ -98,21 +130,36 @@ class ProductAddUpdate extends Component {
             wrapperCol: { span: 8 }, // 右侧包裹的宽度
         };
 
+        // 头部左侧标题
         const title = (
             <span>
                 <LinkButton>
-                    <Icon type='arrow-left' style={{ fontSize: 18 }} />
+                    <Icon type='arrow-left' style={{ fontSize: 18 }} onClick={() => this.props.history.goBack()} />
                 </LinkButton>
-                <span>添加商品</span>
+                <span>{this.isUpdate ? '修改商品' : '添加商品'}</span>
             </span>
         )
+
+        // 用来接收级联分类ID的数组
+        const categoryIds = [];
+        if(this.isUpdate){//修改
+            // 商品是一个一级分类的商品
+            if(this.product.pCategoryId === '0'){
+                categoryIds.push(this.product.categoryId);
+            }else{
+                // 商品是一个二级分类的商品
+                categoryIds.push(this.product.pCategoryId);
+                categoryIds.push(this.product.categoryId);
+            }   
+        }
+
         return (
             <Card title={title}>
                 <Form {...formItemLayout}>
                     <Item label='商品名称'>
                         {
                             getFieldDecorator('name', {
-                                initialValue: '',
+                                initialValue: this.product.name,
                                 rules: [
                                     { required: true, message: '商品名称必须输入' }
                                 ]
@@ -122,7 +169,7 @@ class ProductAddUpdate extends Component {
                     <Item label='商品描述'>
                         {
                             getFieldDecorator('desc', {
-                                initialValue: '',
+                                initialValue: this.product.desc,
                                 rules: [
                                     { required: true, message: '商品描述必须输入' }
                                 ]
@@ -132,7 +179,7 @@ class ProductAddUpdate extends Component {
                     <Item label='商品价格'>
                         {
                             getFieldDecorator('price', {
-                                initialValue: '',
+                                initialValue: this.product.price,
                                 rules: [
                                     { required: true, message: '商品价格必须输入' },
                                     { validator: this.validatorPrice }
@@ -141,13 +188,21 @@ class ProductAddUpdate extends Component {
                         }
                     </Item>
                     <Item label='商品分类'>
-                        <Cascader
-                            options={this.state.options}
-                            loadData={this.loadData}
-                        />
+                        {
+                            getFieldDecorator('categoryIds', {
+                                initialValue: categoryIds,
+                                rules: [
+                                    { required: true, message: '商品分类必须输入' }
+                                ]
+                            })(<Cascader
+                                options={this.state.options}
+                                loadData={this.loadData}
+                            />)
+                        }
+
                     </Item>
                     <Item label='商品图片'>
-                        商品图片
+                        <PicturesWall ref={this.pw}/>
                     </Item>
                     <Item label='商品详情'>
                         商品详情
@@ -162,3 +217,11 @@ class ProductAddUpdate extends Component {
 }
 
 export default Form.create()(ProductAddUpdate);
+
+/**
+ * 1.子组件调用父组件的方法：将父组件的方法以函数的形式传递给子组件，子组件就可以调用
+ * 2.父组件调用子组件的方法：在父组件中通过ref得到子组件标签对象（也就是组件对象），调用其方法
+ *      创建用来保存ref标识的标签对象的容器:this.pw = React.createRef();
+ *      <PicturesWall ref={this.pw}/>
+ *      const imgs = this.pw.current.getImgs();
+ */
---
+++ src/pages/product/picturesWall.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Upload, Icon, Modal, message } from 'antd';
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+/**
+ * 用于图片上传的组件
+ */
+export default class PicturesWall extends React.Component {
+    state = {
+        previewVisible: false, // 标识是否显示大图预览Modal
+        previewImage: '', // 大图的url
+        fileList: [
+            // {
+            //     uid: '-1', // 每个file都有自己唯一的ID
+            //     name: 'image.png', // 图片文件名
+            //     status: 'done', // 图片状态：down-已上传，uploading-正在上传中，error-上传错误，removed-已删除
+            //     url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png', // 图片地址
+            // }
+        ],
+    };
+
+    // 获取所有已上传图片文件名的数组
+    getImgs = () => {
+        return this.state.fileList.map(file => file.name);
+    }
+
+    // 隐藏Modal
+    handleCancel = () => this.setState({ previewVisible: false });
+
+    // 预览
+    handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        this.setState({
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+        });
+    };
+
+    /**
+     * file：当前操作的图片文件（上传/删除）
+     * fileList:所有上传图片文件对象的数组
+     */ 
+    handleChange = ({ file, fileList }) => {
+        // 一旦上传成功，将当前上传的file的信息修正
+        if(file.status === 'done'){
+            const result = file.response; 
+            if(result.status === 0){
+                message.success('上传图片成功');
+                const {name, url} = result.data;
+                fileList[fileList.length - 1].name = name;
+                fileList[fileList.length - 1].url = url;
+            }else{
+                message.error('上传图片失败');
+            }
+        }
+        // 在操作(上传/删除)过程中更新fileList状态
+        this.setState({ fileList })
+    };
+
+    render() {
+        const { previewVisible, previewImage, fileList } = this.state;
+        const uploadButton = (
+            <div>
+                <Icon type="plus" />
+                <div>Upload</div>
+            </div>
+        );
+        return (
+            <div>
+                <Upload
+                    action="/manage/img/upload" // 图片上传的地址
+                    accept='image/*' // 接收文件类型,只接收图片格式
+                    name='image' // 图片上传时请求参数的名字
+                    listType="picture-card"
+                    fileList={fileList} // 用来指定所有已上传文件的列表
+                    onPreview={this.handlePreview}
+                    onChange={this.handleChange}
+                >
+                    {fileList.length >= 8 ? null : uploadButton}
+                </Upload>
+                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </div>
+        );
+    }
+}
